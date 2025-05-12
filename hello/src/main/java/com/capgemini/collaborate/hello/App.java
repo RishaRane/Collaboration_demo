b455c7b9aa765e78883f850dc222c66d8900b93c
RishaRane <risharane6@gmail.com>
2025-05-12 15:16:50 +0530

    First Commit - Risha

---
+++ hello/src/main/java/com/capgemini/collaborate/hello/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.collaborate.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Main Branch - Risha" );
+    }
+}
